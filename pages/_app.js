cd9aefe7de259216d0421561802bb254bd66e3d9
Jonny (PC) <jonathanaingall@gmail.com>
2021-04-01 22:00:39 +0100

    Add next-auth with hardcoded user

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,36 @@
-import '../styles/global.css'
-import '../styles/prism-theme-night-owl.css'
-import Layout from '../components/Layout'
-import siteData from '../siteconfig'
-import { MDXProvider } from '@mdx-js/react'
-import CodeBlock from '../components/CodeBlock'
-import AnchorTag from '../components/AnchorTag'
-import Image from '../components/Image'
+import "../styles/global.css";
+import "../styles/prism-theme-night-owl.css";
+import Layout from "../components/Layout";
+import siteData from "../siteconfig";
+import { MDXProvider } from "@mdx-js/react";
+import { Provider } from "next-auth/client";
+import CodeBlock from "../components/CodeBlock";
+import AnchorTag from "../components/AnchorTag";
+import Image from "../components/Image";
 
 const mdComponents = {
-    a: props => <AnchorTag {...props} />,
-    code: CodeBlock,
-    img: (props) => <div className="nextImageWrapper"><Image  {...props} /></div>,
-    Image: (props) => <div className="nextImageWrapper"><Image  {...props} /></div>
-}
+  a: (props) => <AnchorTag {...props} />,
+  code: CodeBlock,
+  img: (props) => (
+    <div className="nextImageWrapper">
+      <Image {...props} />
+    </div>
+  ),
+  Image: (props) => (
+    <div className="nextImageWrapper">
+      <Image {...props} />
+    </div>
+  ),
+};
 
 export default function App({ Component, pageProps }) {
   return (
     <MDXProvider components={mdComponents}>
       <Layout siteData={siteData}>
-        <Component {...pageProps} />
+        <Provider session={pageProps.session}>
+          <Component {...pageProps} />
+        </Provider>
       </Layout>
-    </MDXProvider>  
-  )
+    </MDXProvider>
+  );
 }
---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,16 @@
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+
+export default NextAuth({
+  providers: [
+    Providers.GitHub({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      return user.name === "ingalless";
+    },
+  },
+});
